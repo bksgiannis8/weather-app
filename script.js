8435f4bb04b78ca030f24997d4006f55c598208a
giannis <bkgiannis8@gmail.com>
2022-11-05 04:15:48 +0200

    Make the info appear on the table

--- script.js
+++ script.js
@@ -2,21 +2,25 @@ weatherKey = '106051cd462aed4cf0d52e78ca2ccc80';
 giphyKey = 'O25fU39PYEF6iHt9d8r9QrSTY4mIaKAB'
 const img = document.querySelector('img');
 const button = document.getElementById('search');
+const cellCity = document.getElementById('city');
+const cellDegree = document.getElementById('degrees')
+const cellHumidity = document.getElementById('humidity')
 
-let sky = ""
-let degrees = ""
-let humidity = ""
+
 
 button.addEventListener('click', () => {
     const city = document.getElementById("input").value;
+    cellCity.innerHTML=city
 
     //function to get the data through an API
     async function getData () {
         const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+city+'&APPID='+weatherKey, {mode:"cors"})
         const weatherData = await response.json()
-        degrees = await weatherData.main.temp
-        humidity = await weatherData.main.humidity
-        sky = await weatherData.weather[0].main 
+        const degrees = await weatherData.main.temp
+        cellDegree.innerHTML = degrees+' '+'Fahrenheit'
+        const humidity = await weatherData.main.humidity
+        cellHumidity.innerHTML = humidity+' '+'%'
+        const sky = await weatherData.weather[0].main 
 
         // function to get a gif related to the weather condition
         async function getImage() {
